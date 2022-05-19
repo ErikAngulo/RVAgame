c7fbc3bb1c961f81da53a0f6ec2d005dc25a70e7
Erik <48107201+ErikAngulo@users.noreply.github.com>
2022-05-19 18:59:38 +0200

    Add how-to-play instruction videos

--- Chameleight/Assets/Scripts/ReadConfig.cs
+++ Chameleight/Assets/Scripts/ReadConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class ReadConfig : MonoBehaviour
 {
@@ -12,12 +14,16 @@ public class ReadConfig : MonoBehaviour
     public HoldController holdController;
     public ButtonController buttonController;
     public gameController gamecontroller;
-    public TextMeshProUGUI text;
-
+    private TextMeshProUGUI _text;
+    private TextMeshProUGUI _leftText;
+    private TextMeshProUGUI _rightText;
+    private VideoPlayer _leftVideo;
+    private VideoPlayer _rightVideo;
     private string _ball_game = "MainScene";
     private string _shooting_game = "Chameleight_Scenary";
     private string _score_scene = "GameOverScene";
     private string _instruction_scene = "InstructionsScene";
+    private string _controller_scene = "ControllerInstructScene";
 
     void Start()
     {
@@ -29,13 +35,13 @@ public class ReadConfig : MonoBehaviour
             gamecontroller.playTime = StaticClass.Time;
         }
         else if (SceneManager.GetActiveScene().name.Equals(_score_scene)){
-            text = GameObject.Find("ResultsText").GetComponent<TextMeshProUGUI>();
-            text.text = StaticClass.scoreText;
+            _text = GameObject.Find("ResultsText").GetComponent<TextMeshProUGUI>();
+            _text.text = StaticClass.scoreText;
         }
         else if (SceneManager.GetActiveScene().name.Equals(_instruction_scene)){
-            text = GameObject.Find("InstructionText").GetComponent<TextMeshProUGUI>();
+            _text = GameObject.Find("InstructionText").GetComponent<TextMeshProUGUI>();
             if (StaticClass.SelectedGameScene.Equals(_ball_game)){
-                text.text = "" +
+                _text.text = "" +
                 "A red or blue ball will appear at the floor."
                 + System.Environment.NewLine +
                 "You must throw it to the button of the same color as the ball."
@@ -44,13 +50,13 @@ public class ReadConfig : MonoBehaviour
                 + System.Environment.NewLine +
                 "This steps must be followed until all balls are used."
                 + System.Environment.NewLine +
-                "In the next screen, you can select how many balls will be available to throw."
+                "In the configuration screen, you can select how many balls will be available to throw."
                 + System.Environment.NewLine +
                 "When the game finishes, game statistics will appear and you will have the option to play again if wished."
                 + System.Environment.NewLine;
             }
             else if (StaticClass.SelectedGameScene.Equals(_shooting_game)){
-                text.text = "" +
+                _text.text = "" +
                 "A gun will appear, which you will use to shoot."
                 + System.Environment.NewLine +
                 "You must shoot to the target that has a light on."
@@ -61,7 +67,7 @@ public class ReadConfig : MonoBehaviour
                 + System.Environment.NewLine +
                 "After a period of time, a bulb of a target will turn on and you must repeat the process."
                 + System.Environment.NewLine +
-                "In the next screen, you can select how many time you want to play."
+                "In the configuration screen, you can select how many time you want to play."
                 + System.Environment.NewLine +
                 "Besides, a 'movement' option can be selected with 'Yes'."
                 + System.Environment.NewLine +
@@ -69,6 +75,50 @@ public class ReadConfig : MonoBehaviour
                 + System.Environment.NewLine +
                 "When the game finishes, game statistics will appear and you will have the option to play again if wished."
                 + System.Environment.NewLine;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name.Equals(_controller_scene)){
+            _text = GameObject.Find("InstructionText").GetComponent<TextMeshProUGUI>();
+            _leftText = GameObject.Find("LeftText").GetComponent<TextMeshProUGUI>();
+            _rightText = GameObject.Find("RightText").GetComponent<TextMeshProUGUI>();
+            _leftVideo = GameObject.Find("LeftVideo").GetComponent<VideoPlayer>();
+            _rightVideo = GameObject.Find("RightVideo").GetComponent<VideoPlayer>();
+
+            if (StaticClass.SelectedGameScene.Equals(_ball_game)){
+                _text.text = "" +
+                "You can play with both controllers."
+                + System.Environment.NewLine +
+                "Pick the ball with hand trigger."
+                + System.Environment.NewLine +
+                "You can also play using your hands."
+                + System.Environment.NewLine +
+                "To do so, leave the controllers in a table when the game starts."
+                + System.Environment.NewLine +
+                "Check the videos next to this text to know the gestures to throw the ball."
+                + System.Environment.NewLine;
+
+                _leftText.text = "Using hands";
+                _leftVideo.clip = (VideoClip)Resources.Load("BallHand");
+                _rightText.text = "Using controller";
+                _rightVideo.clip = (VideoClip)Resources.Load("BallController");
+
+            }
+            else if (StaticClass.SelectedGameScene.Equals(_shooting_game)){
+                _text.text = "" +
+                "You will play with the controller you select in the next screen."
+                + System.Environment.NewLine +
+                "You can move in the platform with the joystick of the left controller."
+                + System.Environment.NewLine +
+                "Move the controller to move the gun."
+                + System.Environment.NewLine +
+                "When pressing index trigger you will shoot the gun."
+                + System.Environment.NewLine;
+
+                _rightText.text = "Using controller";
+                _rightVideo.clip = (VideoClip)Resources.Load("ShootingController");
+                _leftVideo.enabled = false;
+                _leftText.text = "";
+                GameObject.Find("LeftImage").GetComponent<RawImage>().enabled = false;
             }
         }
     }
